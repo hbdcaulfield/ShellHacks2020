1ef257502c3fefc9edfe3d9933da4c2ab874d876
hbdcaulfield <hditarando@gmail.com>
2020-09-26 21:35:38 -0400

    commit

--- php/write.php
+++ php/write.php
@@ -1,10 +1,10 @@
 <?php
-              
+
 if(isset($_POST['BusName']))
 {
 $data=$_POST['BusName'];
 
-$fp = fopen('data.txt', 'a');
+$fp = fopen('/home/hdita/Documents/ShellHacks2020/ShellHacks2020/data.txt', 'a');
 fwrite($fp, $data);
 fclose($fp);
 }
