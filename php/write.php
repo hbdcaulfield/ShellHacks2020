5f62a361c5f6d289060d2f431685a1146f3bab81
hbdcaulfield <hditarando@gmail.com>
2020-09-26 21:17:56 -0400

    getting there...

--- php/write.php
+++ php/write.php
@@ -1,15 +1,11 @@
 <?php
-if(isset($_POST['BusName']) && isset($_POST['BusAdd'])) {
-    $data = $_POST['BusName'] . '-' . $_POST['BusAdd'] . "\r\n";
-    $ret = file_put_contents('data.txt', $data, FILE_APPEND | LOCK_EX);
-    if($ret === false) {
-        die('There was an error writing this file');
-    }
-    else {
-        echo "$ret bytes written to file";
-    }
-}
-else {
-   die('no post data to process');
+              
+if(isset($_POST['BusName']))
+{
+$data=$_POST['BusName'];
+
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
 }
 ?>
